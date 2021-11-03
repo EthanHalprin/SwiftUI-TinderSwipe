c099521efa5dbab84ff26ed39a81169a0afbf277
Ethan Halprin <ethan.halprin@gmail.com>
2021-11-03 20:58:35 +0200

    Extension for DraggableCardView to ease pressure on body

--- SwiftUI-TinderSwipe/DraggableCardView.swift
+++ SwiftUI-TinderSwipe/DraggableCardView.swift
@@ -22,24 +22,29 @@ struct DraggableCardView: View {
                      imageName  : image,
                      borderColor: outlineColor)
                 .offset(drag)
-                .gesture(
-                    DragGesture()
-                        .onChanged {
-                            print("(\($0.translation.width) ,\($0.translation.height))")
-                            if $0.translation.height > 0 {
-                                self.drag = $0.translation
-                            }
-                        }
-                        .onEnded {
-                            let totalXDrag = $0.translation.width > 0 ? $0.translation.width : $0.translation.width * (-1.0)
-                            if totalXDrag >= UIScreen.main.bounds.width / 2.0 - 40.0 {
-                                print("DISPELL CARD !!!!!!!!!")
-                                isShowing = false
-                            } else {
-                                withAnimation { drag = CGSize(width: 15.0, height: 15.0) }
-                            }
-                        }
-                )
+                .gesture(DragGesture()
+                            .onChanged { self.handleDrag($0) }
+                            .onEnded   { self.handleEnd($0)  })
+        }
+    }
+}
+
+extension DraggableCardView {
+    
+    func handleDrag(_ dragGesture: DragGesture.Value) {
+        print("(\(dragGesture.translation.width) ,\(dragGesture.translation.height))")
+        if dragGesture.translation.height > 0 {
+            self.drag = dragGesture.translation
+        }
+    }
+    
+    func handleEnd(_ dragGesture: DragGesture.Value) {
+        let totalXDrag = dragGesture.translation.width > 0 ? dragGesture.translation.width : dragGesture.translation.width * (-1.0)
+        if totalXDrag >= UIScreen.main.bounds.width / 2.0 - 40.0 {
+            print("DISPELL CARD !!!!!!!!!")
+            isShowing = false
+        } else {
+            withAnimation { drag = CGSize(width: 15.0, height: 15.0) }
         }
     }
 }
